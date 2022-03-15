0b2f06d3a83fd33f13bea3aec396f0d210263dfa
lev <lkorasik83@gmail.com>
2022-03-15 18:09:01 +0500

    Add toolbar

--- app/src/main/java/com/lkorasik/doublehabits/ui/MainActivity.kt
+++ app/src/main/java/com/lkorasik/doublehabits/ui/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.lkorasik.doublehabits.ui
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.google.android.material.navigation.NavigationView
 import com.lkorasik.doublehabits.Habit
 import com.lkorasik.doublehabits.R
+import com.lkorasik.doublehabits.databinding.ActivityMainRBinding
+
 
 class MainActivity: AppCompatActivity() {
     private lateinit var habitsListFragment: HabitsListFragment
+    private lateinit var binding: ActivityMainRBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_r)
+        binding = ActivityMainRBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         habitsListFragment = HabitsListFragment.newInstance()
 
@@ -22,6 +33,49 @@ class MainActivity: AppCompatActivity() {
                 .add(R.id.fragment_host, habitsListFragment)
                 .commit()
         }
+
+        configureNavigationDrawer();
+        configureToolbar();
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return true
+    }
+
+    private fun configureNavigationDrawer() {
+        drawerLayout = binding.drawerLayout
+        val navView = binding.navigationView
+        navView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener { menuItem ->
+            var f: Fragment? = null
+            val itemId = menuItem.itemId
+
+            if (itemId == R.id.home) {
+                f = HabitsListFragment.newInstance()
+            }
+
+            if (f != null) {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragment_host, f)
+                transaction.commit()
+                drawerLayout.closeDrawers()
+                return@OnNavigationItemSelectedListener true
+            }
+            false
+        })
+    }
+
+    private fun configureToolbar() {
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar?.setHomeAsUpIndicator(R.drawable.ic_add)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onBackPressed() {
