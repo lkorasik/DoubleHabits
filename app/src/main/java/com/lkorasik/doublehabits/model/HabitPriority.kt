e474aa62d4d421842f884fa1749438d41bf986cd
lev <lkorasik83@gmail.com>
2022-03-21 15:13:59 +0500

    Refactoring

--- app/src/main/java/com/lkorasik/doublehabits/model/HabitPriority.kt
+++ app/src/main/java/com/lkorasik/doublehabits/model/HabitPriority.kt
@@ -1,17 +1,16 @@
 package com.lkorasik.doublehabits.model
 
-import android.content.Context
 import com.lkorasik.doublehabits.R
 
 enum class HabitPriority {
     LOW, MEDIUM, HIGH;
 
-    fun getRepresentation(context: Context): String {
-        return when (ordinal) {
-            LOW.ordinal -> context.getString(R.string.add_habit_habit_priority_low)
-            MEDIUM.ordinal -> context.getString(R.string.add_habit_habit_priority_medium)
-            HIGH.ordinal -> context.getString(R.string.add_habit_habit_priority_high)
-            else -> throw IllegalStateException("Incorrect ordinal value")
+    val resource: Int
+        get() {
+            return when (this) {
+                LOW -> R.string.add_habit_habit_priority_low
+                MEDIUM -> R.string.add_habit_habit_priority_medium
+                HIGH -> R.string.add_habit_habit_priority_high
+            }
         }
-    }
 }
--- app/src/main/java/com/lkorasik/doublehabits/model/HabitType.kt
+++ app/src/main/java/com/lkorasik/doublehabits/model/HabitType.kt
@@ -1,16 +1,15 @@
 package com.lkorasik.doublehabits.model
 
-import android.content.Context
 import com.lkorasik.doublehabits.R
 
 enum class HabitType {
     REGULAR, HARMFUL;
 
-    fun getRepresentation(context: Context): String {
-        return when (ordinal) {
-            REGULAR.ordinal -> context.getString(R.string.add_habit_habit_type_regular)
-            HARMFUL.ordinal -> context.getString(R.string.add_habit_habit_type_harmful)
-            else -> throw IllegalStateException("Incorrect ordinal value")
+    val resource: Int
+        get() {
+            return when (this) {
+                REGULAR -> R.string.add_habit_habit_type_regular
+                HARMFUL -> R.string.add_habit_habit_type_harmful
+            }
         }
-    }
 }
