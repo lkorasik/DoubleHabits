b62cef3479c48ffc00abfc7e11d19497c0c4474e
lev <lkorasik83@gmail.com>
2022-03-21 17:02:21 +0500

    Update MainActivity.kt

--- app/src/main/java/com/lkorasik/doublehabits/ui/MainActivity.kt
+++ app/src/main/java/com/lkorasik/doublehabits/ui/MainActivity.kt
@@ -2,29 +2,22 @@ package com.lkorasik.doublehabits.ui
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.core.view.GravityCompat
-import androidx.fragment.app.commit
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.lkorasik.doublehabits.IntentKeys
-import com.lkorasik.doublehabits.model.HabitType
 import com.lkorasik.doublehabits.R
 import com.lkorasik.doublehabits.databinding.ActivityMainBinding
 import com.lkorasik.doublehabits.model.Habit
+import com.lkorasik.doublehabits.model.HabitType
 
 
 class MainActivity: AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-//    private val habitListBaseFragment = HabitListBaseFragment.newInstance()
-//    private val aboutFragment = AboutFragment.newInstance()
-//    private val editorFragment = HabitEditorFragment.newInstance()
 
     private lateinit var navController: NavController
 
@@ -36,11 +29,7 @@ class MainActivity: AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        configureNavigationDrawer()
         configureToolbar()
-
-        if (savedInstanceState == null)
-            initFragmentManager()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
         navController = navHostFragment.navController
@@ -49,57 +38,6 @@ class MainActivity: AppCompatActivity() {
 
         val appBarConfig = AppBarConfiguration(navController.graph, drawerLayout = binding.drawerLayout)
         binding.toolbar.setupWithNavController(navController, appBarConfig)
-    }
-
-    private fun initFragmentManager() {
-//        supportFragmentManager.commit {
-//            addToBackStack(null)
-//            add(R.id.fragment_host, habitListBaseFragment)
-//        }
-    }
-
-    private fun configureNavigationDrawer() {
-//        val drawer = setupDrawerToggle()
-//        binding.drawerLayout.addDrawerListener(drawer)
-//
-//        binding.navigationView.setNavigationItemSelectedListener {
-//            when(it.itemId) {
-//                R.id.home -> navController.navigate(R.id.action_aboutFragment_to_habitListBaseFragment)
-//                R.id.about -> navController.navigate(R.id.action_habitListBaseFragment_to_aboutFragment)
-//            }
-//
-//            return@setNavigationItemSelectedListener true
-//        }
-
-//        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
-//            binding.navigationView.setCheckedItem(menuItem)
-//
-//            val fragment = when(menuItem.itemId) {
-//                R.id.home -> habitListBaseFragment
-//                R.id.about -> aboutFragment
-//                else -> return@setNavigationItemSelectedListener false
-//            }
-//
-//            supportFragmentManager.commit {
-//                replace(R.id.fragment_host, fragment)
-//            }
-//
-//            binding.drawerLayout.closeDrawers()
-//
-//            return@setNavigationItemSelectedListener true
-//        }
-    }
-
-    private fun setupDrawerToggle(): ActionBarDrawerToggle {
-        val toggle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayout,
-            binding.toolbar,
-            R.string.main_activity_open_navigation_drawer,
-            R.string.main_activity_close_navigation_drawer)
-        toggle.isDrawerIndicatorEnabled = true
-        toggle.syncState()
-        return toggle
     }
 
     private fun configureToolbar() {
@@ -132,20 +70,10 @@ class MainActivity: AppCompatActivity() {
 
     fun createHabit() {
         navController.navigate(R.id.habitEditorFragment)
-//        supportFragmentManager.commit {
-//            addToBackStack(null)
-//            replace(R.id.fragment_host, editorFragment, EDITOR_FRAGMENT_TAG)
-//        }
     }
 
     fun editHabit(habit: Habit, position: Int) {
-//        val fragment = HabitEditorFragment.newInstance(habit, position)
         navController.navigate(R.id.habitEditorFragment, bundleOf(IntentKeys.Habit to habit, IntentKeys.Position to position))
-
-//        supportFragmentManager.commit {
-//            addToBackStack(null)
-//            replace(R.id.fragment_host, fragment, EDITOR_FRAGMENT_TAG)
-//        }
     }
 
     fun saveHabit(habit: Habit, position: Int) {
@@ -154,7 +82,6 @@ class MainActivity: AppCompatActivity() {
             HabitType.HARMFUL -> habitsHarmful[position] = habit
         }
         navController.popBackStack()
-//        supportFragmentManager.popBackStack()
     }
 
     fun move(type: HabitType, position: Int): Int {
@@ -180,10 +107,6 @@ class MainActivity: AppCompatActivity() {
             HabitType.HARMFUL -> habitsHarmful.add(habit)
         }
         navController.popBackStack()
-//        navController.navigate(R.id.habitListBaseFragment)
-//        navController.clearBackStack(R.id.habitListBaseFragment)
-
-//        supportFragmentManager.popBackStack()
     }
 
     companion object {
